04de49f02a03f9e877ead1a9686f0b1bdaed3243
Giannina Ansaldo <gianni.ansaldo@gmail.com>
2024-07-20 20:37:20 -0400

    última modificación agregando jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-//tooltip y alerta
+//tooltip y alerta de correo enviado
 document.addEventListener('DOMContentLoaded', function () {
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
@@ -9,16 +9,36 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-//Cambio color al hacer click
-$(document).ready(function(){
-    $("#inicio").on("click", function(){
-        $(this).css("color", "cyan");
+//Se añade subrayado al hacer click en links de secciones
+$(document).ready(function () {
+    $('.navbar-nav .nav-link').click(function () {
+        $('.navbar-nav .nav-link').removeClass('active');
+        $(this).addClass('active');
+    });
+});
+
+//aparecer y desaparecer p de cards haciendo click
+$(document).ready(function () {
+    $("h5").click(function () {
+        $(".p-card").toggle();
+    });
+});
+
+//cambia color del párrafo al hacer doble click
+$(document).ready(function () {
+    $("#p-plane").on("dblclick", function () {
+        $(this).css("color", "darkgray");
     });
 });
 
 $(document).ready(function () {
-   $("#inicio").click(function () {
-    $(this).css("color", "cyan");
-   });
+    $("#p-mountain").on("dblclick", function () {
+        $(this).css("color", "darkgray");
+    });
 });
 
+$(document).ready(function () {
+    $("#p-route").on("dblclick", function () {
+        $(this).css("color", "darkgray");
+    });
+});
